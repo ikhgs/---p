8859d4b6b41d62e2d6db4cc754854f3a09ed4b27
ikhgs <mathematiques45@gmail.com>
2024-09-10 22:36:29 +0300

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,9 @@
+const spotify = require('./spotify');
+const gemini = require('./gemini');
+// Ajoutez d'autres commandes ici
+
+module.exports = {
+  spotify,
+  gemini,
+  // Exposez d'autres commandes ici
+};
