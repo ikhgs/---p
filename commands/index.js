44edf51bb6a4ff4d9526ddd61daad6c7fbae48fb
ikhgs <mathematiques45@gmail.com>
2024-09-10 23:56:45 +0300

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -3,6 +3,7 @@ const gemini = require('./gemini');
 const gpt4 = require('./gpt4');
 const bacc = require('./bacc');
 const help = require('./help');
+const par = require('./par');
 // Ajoutez d'autres commandes ici
 
 module.exports = {
@@ -11,5 +12,6 @@ module.exports = {
   gpt4,
   help,
   bacc,
+  par,
   // Exposez d'autres commandes ici
 };
