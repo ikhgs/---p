4cf2917d8387febfcd35f1f7c310489f7e2c5e91
ikhgs <mathematiques45@gmail.com>
2024-09-10 23:09:59 +0300

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,7 @@
 const spotify = require('./spotify');
 const gemini = require('./gemini');
 const gpt4 = require('./gpt4');
-
+const bacc = require('./bacc');
 const help = require('./help');
 // Ajoutez d'autres commandes ici
 
